04a26e1b66f24912875041d1da0bfb6b656e62ff
XinyiHuang3 <xhuang3@stuy.edu>
2017-11-14 01:02:31 -0500

    higher attributes & about methods added

--- v3/Archer.java
+++ v3/Archer.java
@@ -1,2 +1,10 @@
 public class Archer extends Protagonist{
+        public Archer(){
+	super();
+	strength += 10;
+    }
+
+    public static String about(){
+	return "Archer has a higher strength.";
+    }
 }
--- v3/Wizard.java
+++ v3/Wizard.java
@@ -1,2 +1,10 @@
 public class Wizard extends Protagonist{
+        public Wizard(){
+	super();
+	HP += 10;
+    }
+
+    public static String about(){
+	return "Wizard has a higher HP.";
+    }
 }
